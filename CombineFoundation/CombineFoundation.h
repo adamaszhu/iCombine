547927d0586e0e7dbfae2ae92bcede427494de09
Adamas Zhu <>
2021-08-04 21:43:31 +1000

    Create the project

---
+++ CombineFoundation/CombineFoundation.h
@@ -0,0 +1,18 @@
+//
+//  CombineFoundation.h
+//  CombineFoundation
+//
+//  Created by Adamas Zhu on 4/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CombineFoundation.
+FOUNDATION_EXPORT double CombineFoundationVersionNumber;
+
+//! Project version string for CombineFoundation.
+FOUNDATION_EXPORT const unsigned char CombineFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CombineFoundation/PublicHeader.h>
+
+
