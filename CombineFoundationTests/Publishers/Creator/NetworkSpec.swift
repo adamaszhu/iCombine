74cd6e674867df4d0a11edeb47d89fdbc96e873b
Leon Nguyen <>
2021-08-11 22:41:44 +1000

    Implement Network and run all tests for native Combine

---
+++ CombineFoundationTests/Publishers/Creator/NetworkSpec.swift
@@ -0,0 +1,31 @@
+//
+//  NetworkSpec.swift
+//  CombineFoundationTests
+//
+//  Created by Leon Nguyen on 11/8/21.
+//
+
+import Nimble
+import Quick
+#if COMBINE
+import Combine
+import Foundation
+#else
+@testable import CombineFoundation
+#endif
+
+final class NetworkSpec: QuickSpec {
+    override func spec() {
+        describe("calls DataTaskPublisher") {
+            context("with an URL") {
+                it("should receive value") {
+                    let url = URL(string: "https://www.google.com")
+                    let dataTaskPub: URLSession.CombineDataTaskPublisher = URLSession.shared.dataTaskPublisher(for: url!)
+                    let result = dataTaskPub.test()
+                    expect(result.hasFinished).toEventually(beFalse())
+                    expect(result.outputs.last?.data.count).toEventually(beGreaterThan(0))
+                }
+            }
+        }
+    }
+}
