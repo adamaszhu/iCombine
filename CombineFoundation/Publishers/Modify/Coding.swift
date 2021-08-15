6a9b0fb5d6881af6af674e1666d9791bff4d1e70
Adamas Zhu <>
2021-08-15 21:34:58 +1000

    Add modify publishers

---
+++ CombineFoundation/Publishers/Modify/Coding.swift
@@ -0,0 +1,108 @@
+//
+//  Coding.swift
+//  CombineFoundation
+//
+//  Created by Adamas Zhu on 15/8/21.
+//
+
+import RxCocoa
+import RxSwift
+#if canImport(Combine)
+import Combine
+#endif
+
+public enum CodingError: Error {
+    case decoding
+    case encoding
+}
+
+public protocol TopLevelDecoder {
+    
+    associatedtype Input
+    
+    func decode<T>(_ type: T.Type, from: Self.Input) throws -> T where T : Decodable
+}
+
+public protocol TopLevelEncoder {
+    
+    associatedtype Output
+    
+    func encode<T>(_ value: T) throws -> Self.Output where T : Encodable
+}
+
+extension JSONDecoder: TopLevelDecoder {}
+extension JSONEncoder: TopLevelEncoder {}
+
+extension Publishers {
+    
+    public struct Decode<Upstream, Output, Coder> : Publisher where Upstream : Publisher, Output : Decodable, Coder : TopLevelDecoder, Upstream.Output == Coder.Input {
+        
+        public var observable: Any
+        
+        /// The kind of errors this publisher might publish.
+        ///
+        /// Use `Never` if this `Publisher` does not publish errors.
+        public typealias Failure = Error
+        
+        public init(upstream: Upstream, decoder: Coder) {
+            #if canImport(Combine)
+            if #available(iOS 14, *),
+                let publisher = upstream.observable as? Combine.AnyPublisher<Upstream.Output, Upstream.Failure> {
+                let mappedObserable: Combine.AnyPublisher<Output, Failure> = publisher.tryMap({ value in
+                    do {
+                        return try decoder.decode(Output.self, from: value)
+                    } catch {
+                        throw CodingError.decoding
+                    }
+                }).eraseToAnyPublisher()
+                observable = mappedObserable
+                return
+            }
+            #endif
+            if let upstreamObservable = upstream.observable as? Observable<Upstream.Output> {
+                let mappedObservable: Observable<Output> = upstreamObservable.map { value in
+                    do {
+                        return try decoder.decode(Output.self, from: value)
+                    } catch {
+                        throw CodingError.decoding
+                    }
+                }
+                observable = mappedObservable
+            } else {
+                fatalError("failed to init Decode")
+            }
+        }
+    }
+    
+    // public struct Encode<Upstream, Coder> : Publisher where Upstream : Publisher, Coder : TopLevelEncoder, Upstream.Output : Encodable {
+    
+    // public var observable: Observable<Coder.Output>
+    
+    /// The kind of errors this publisher might publish.
+    ///
+    /// Use `Never` if this `Publisher` does not publish errors.
+    // public typealias Failure = Error
+    
+    /// The kind of values published by this publisher.
+    // public typealias Output = Coder.Output
+    
+    // public init(upstream: Upstream, encoder: Coder)
+    // }
+}
+
+extension Publisher {
+    
+    /// Decodes the output from upstream using a specified `TopLevelDecoder`.
+    /// For example, use `JSONDecoder`.
+    public func decode<Item, Coder>(type: Item.Type, decoder: Coder) -> Publishers.Decode<Self, Item, Coder> where Item : Decodable, Coder : TopLevelDecoder, Self.Output == Coder.Input {
+        return Publishers.Decode(upstream: self, decoder: decoder)
+    }
+}
+
+
+extension Publisher where Self.Output : Encodable {
+    
+    /// Encodes the output from upstream using a specified `TopLevelEncoder`.
+    /// For example, use `JSONEncoder`.
+    // public func encode<Coder>(encoder: Coder) -> Publishers.Encode<Self, Coder> where Coder : TopLevelEncoder
+}
---
+++ CombineFoundation/Publishers/Modify/RemoveDuplicates.swift
@@ -0,0 +1,124 @@
+//
+//  RemoveDuplicates.swift
+//  CombineFoundation
+//
+//  Created by Adamas Zhu on 15/8/21.
+//
+
+import RxSwift
+import RxCocoa
+#if canImport(Combine)
+import Combine
+#endif
+
+extension Publishers {
+
+    /// A publisher that publishes only elements that don’t match the previous element.
+    public struct RemoveDuplicates<Upstream> : Publisher where Upstream : Publisher {
+        
+        public let observable: Any
+
+        /// The kind of values published by this publisher.
+        public typealias Output = Upstream.Output
+
+        /// The kind of errors this publisher might publish.
+        ///
+        /// Use `Never` if this `Publisher` does not publish errors.
+        public typealias Failure = Upstream.Failure
+
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+
+        /// A closure to evaluate whether two elements are equivalent, for purposes of filtering.
+        public let predicate: (Upstream.Output, Upstream.Output) -> Bool
+
+        /// Creates a publisher that publishes only elements that don’t match the previous element, as evaluated by a provided closure.
+        /// - Parameter upstream: The publisher from which this publisher receives elements.
+        /// - Parameter predicate: A closure to evaluate whether two elements are equivalent, for purposes of filtering. Return `true` from this closure to indicate that the second element is a duplicate of the first.
+        public init(upstream: Upstream,
+                    predicate: @escaping (Publishers.RemoveDuplicates<Upstream>.Output, Publishers.RemoveDuplicates<Upstream>.Output) -> Bool) {
+            self.upstream = upstream
+            self.predicate = predicate
+            #if canImport(Combine)
+            if #available(iOS 14, *),
+                let publisher = upstream.observable as? Combine.AnyPublisher<Upstream.Output, Failure> {
+                observable = Combine.Publishers.RemoveDuplicates(upstream: publisher, predicate: predicate)
+                    .eraseToAnyPublisher()
+                return
+            }
+            #endif
+            if let upstreamObservable = upstream.observable as? Observable<Upstream.Output> {
+                observable = upstreamObservable.distinctUntilChanged(predicate)
+            } else {
+                fatalError("failed to init RemoveDuplicates")
+            }
+        }
+    }
+
+    /// A publisher that publishes only elements that don’t match the previous element, as evaluated by a provided error-throwing closure.
+    public struct TryRemoveDuplicates<Upstream> : Publisher where Upstream : Publisher {
+
+        public let observable: Any
+        
+        /// The kind of values published by this publisher.
+        public typealias Output = Upstream.Output
+
+        /// The kind of errors this publisher might publish.
+        ///
+        /// Use `Never` if this `Publisher` does not publish errors.
+        public typealias Failure = Error
+
+        /// The publisher from which this publisher receives elements.
+        public let upstream: Upstream
+
+        /// An error-throwing closure to evaluate whether two elements are equivalent, for purposes of filtering.
+        public let predicate: (Upstream.Output, Upstream.Output) throws -> Bool
+
+        /// Creates a publisher that publishes only elements that don’t match the previous element, as evaluated by a provided error-throwing closure.
+        /// - Parameter upstream: The publisher from which this publisher receives elements.
+        /// - Parameter predicate: An error-throwing closure to evaluate whether two elements are equivalent, for purposes of filtering. Return `true` from this closure to indicate that the second element is a duplicate of the first. If this closure throws an error, the publisher terminates with the thrown error.
+        public init(upstream: Upstream,
+                    predicate: @escaping (Publishers.TryRemoveDuplicates<Upstream>.Output, Publishers.TryRemoveDuplicates<Upstream>.Output) throws -> Bool) {
+            self.upstream = upstream
+            self.predicate = predicate
+            #if canImport(Combine)
+            if #available(iOS 14, *),
+                let publisher = upstream.observable as? Combine.AnyPublisher<Upstream.Output, Upstream.Failure> {
+                observable = Combine.Publishers.TryRemoveDuplicates(upstream: publisher, predicate: predicate)
+                    .eraseToAnyPublisher()
+                return
+            }
+            #endif
+            if let upstreamObservable = upstream.observable as? Observable<Upstream.Output> {
+                observable = upstreamObservable.distinctUntilChanged(predicate)
+            } else {
+                fatalError("failed to init TryRemoveDuplicates")
+            }
+        }
+    }
+}
+
+extension Publisher where Self.Output : Equatable {
+
+    /// Publishes only elements that don’t match the previous element.
+    ///
+    /// - Returns: A publisher that consumes — rather than publishes — duplicate elements.
+    public func removeDuplicates() -> Publishers.RemoveDuplicates<Self> {
+        return Publishers.RemoveDuplicates(upstream: self, predicate: { $0 == $1 })
+    }
+}
+
+extension Publisher {
+
+    /// Publishes only elements that don’t match the previous element, as evaluated by a provided closure.
+    /// - Parameter predicate: A closure to evaluate whether two elements are equivalent, for purposes of filtering. Return `true` from this closure to indicate that the second element is a duplicate of the first.
+    public func removeDuplicates(by predicate: @escaping (Self.Output, Self.Output) -> Bool) -> Publishers.RemoveDuplicates<Self> {
+        return Publishers.RemoveDuplicates(upstream: self, predicate: predicate)
+    }
+
+    /// Publishes only elements that don’t match the previous element, as evaluated by a provided error-throwing closure.
+    /// - Parameter predicate: A closure to evaluate whether two elements are equivalent, for purposes of filtering. Return `true` from this closure to indicate that the second element is a duplicate of the first. If this closure throws an error, the publisher terminates with the thrown error.
+    public func tryRemoveDuplicates(by predicate: @escaping (Self.Output, Self.Output) throws -> Bool) -> Publishers.TryRemoveDuplicates<Self> {
+        return Publishers.TryRemoveDuplicates(upstream: self, predicate: predicate)
+    }
+}
