33f0c0981a684a49bf43e8c5c700795a16f4c120
Adamas Zhu <>
2021-08-16 22:44:52 +1000

    Add more tests

---
+++ CombineFoundationTests/Subjects/PassthroughSubjectSpec.swift
@@ -0,0 +1,59 @@
+//
+//  PassthroughSubjectSpec.swift
+//  CombineFoundationTests
+//
+//  Created by Adamas Zhu on 16/8/21.
+//
+
+import Nimble
+import Quick
+
+#if COMBINE
+import Combine
+#else
+@testable import CombineFoundation
+#endif
+
+final class PassthroughSubjectSpec: QuickSpec {
+    
+    override func spec() {
+        describe("calls send(event)") {
+            context("with new value") {
+                it("publish the new value") {
+                    let subject = PassthroughSubject<Int, Never>()
+                    let result = subject.test()
+                    subject.send(1)
+                    expect(result.outputs.last).toEventually(equal(1))
+                }
+            }
+        }
+        describe("calls send(completion)") {
+            context("with finish") {
+                it("publish the ending event") {
+                    let subject = PassthroughSubject<Int, Never>()
+                    let result = subject.test()
+                    subject.send(completion: .finished)
+                    expect(result.hasFinished).toEventually(equal(true))
+                }
+            }
+            context("with error") {
+                it("publish the error") {
+                    let subject = PassthroughSubject<Int, Error>()
+                    let result = subject.test()
+                    subject.send(completion: .failure(TestError.one))
+                    expect(result.failure).toEventuallyNot(beNil())
+                }
+            }
+        }
+        describe("called as a subscriber") {
+            context("with new value") {
+                it("forwards the new value") {
+                    let subject = PassthroughSubject<Int, Never>()
+                    let result = subject.test()
+                    let _ = Just<Int>(1).subscribe(subject)
+                    expect(result.outputs.last).toEventually(equal(1))
+                }
+            }
+        }
+    }
+}
