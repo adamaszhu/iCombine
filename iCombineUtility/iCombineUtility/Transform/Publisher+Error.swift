66b37692e4317c3cf94c9b2f495f67aa9263306c
Leon Nguyen <>
2021-08-29 22:58:28 +1000

    Add additional support for error mapping Add ability for skipping nil values

---
+++ iCombineUtility/iCombineUtility/Transform/Publisher+Error.swift
@@ -0,0 +1,47 @@
+/// Publisher+Error.swift
+/// CombineUtility
+///
+/// - author: Leon Nguyen
+/// - date: 28/8/21
+///
+
+import iCombine
+
+public extension iCombine.Publisher {
+    
+    /// Map any error into a specific error.
+    /// - Parameter error: The error to be mapped into.
+    func mapError<E>(into error: E) -> iCombine.AnyPublisher<Output, E> where E : Error {
+        return mapError { _ in error }
+            .eraseToAnyPublisher()
+    }
+    
+    /// Ignore errors occur in the publisher
+    func ignoreError() -> iCombine.AnyPublisher<Output, Never> {
+        return self.catch { _ in iCombine.AnyPublisher<Output, Never>.empty }
+            .eraseToAnyPublisher()
+    }
+}
+
+#if canImport(Combine)
+
+import Combine
+
+@available(iOS 13.0, *)
+public extension Combine.Publisher {
+    
+    /// Map any error into a specific error.
+    /// - Parameter error: The error to be mapped into.
+    func mapError<E>(into error: E) -> Combine.AnyPublisher<Output, E> where E : Error {
+        return mapError { _ in error }
+            .eraseToAnyPublisher()
+    }
+    
+    /// Ignore errors occur in the publisher
+    func ignoreError() -> Combine.AnyPublisher<Output, Never> {
+        return self.catch { _ in Combine.AnyPublisher<Output, Never>.empty }
+            .eraseToAnyPublisher()
+    }
+}
+
+#endif
